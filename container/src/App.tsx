9b252af1d7c4dcadfba9d2a353d51338e030cee2
Martin Stievenart <stievenart.m@gmail.com>
2023-11-30 15:36:39 +0100

    feat: use child component inside container app

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,7 +1,11 @@
+import { App as ChildApp } from "firstApp/App";
+
 export const App = () => {
   return (
     <>
       <h1>This is the container App !</h1>
+
+      <ChildApp />
     </>
   );
 };
--- first-app/vite.config.ts
+++ first-app/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  federation({
+    name: 'firstApp',
+    filename: 'remoteEntry.js',
+    exposes: {
+      './App': './src/App.tsx',
+    },
+  })],
 })
